19040351beb05c1fcf489fa753b107015085e4d6
Alex Campo <abrettc@gmail.com>
2022-09-12 08:41:47 -0500

    added initial calls to backend with recipe and ingredient

--- client/src/pages/MyPantry.js
+++ client/src/pages/MyPantry.js
@@ -1,7 +1,46 @@
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+
 export default function MyPantry() {
-  return (
+  const [favoriteMeals, setFavoriteMeals] = useState()
+  const [myIngredients, setMyIngredients] = useState()
+
+  const getFavoriteRecipes = async () => {
+    const res = await axios.get(`http://localhost:3001/recipes/`)
+    console.log(res.data)
+    setFavoriteMeals(res.data)
+  }
+
+  const getIngredients = async () => {
+    const res = await axios.get(`http://localhost:3001/ingredients/`)
+    console.log(res.data)
+    setMyIngredients(res.data)
+  }
+
+  useEffect(() => {
+    getFavoriteRecipes()
+    getIngredients()
+  }, [])
+
+  return favoriteMeals ? (
     <div>
-      <h1>My Pantry page</h1>
+      <h1>My Pantry</h1>
+      <h1>My Favorite Meals</h1>
+      {favoriteMeals.map((meal) => (
+        <div>
+          <h2>{meal.recipeName}</h2>
+          <img src={meal.imageUrl} className="recipe-image" />
+          <p>{meal.directions}</p>
+        </div>
+      ))}
+      <h1>My Ingredients</h1>
+      {myIngredients.map((ingredient) => (
+        <div>
+          <h2>{ingredient.name}</h2>
+        </div>
+      ))}
     </div>
+  ) : (
+    <h1>Loading...</h1>
   )
 }
